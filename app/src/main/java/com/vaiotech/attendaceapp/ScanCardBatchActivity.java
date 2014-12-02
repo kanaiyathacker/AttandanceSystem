d9989cc80740c7bf0349044e6dc4c5e7bb7997c8
kanaiyathacker <kanaiyalal.thacker@zonal.com>
2014-12-02 18:21:45 +0000

    Commit

--- app/src/main/java/com/vaiotech/attendaceapp/ScanCardBatchActivity.java
+++ app/src/main/java/com/vaiotech/attendaceapp/ScanCardBatchActivity.java
@@ -12,12 +12,14 @@ import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.os.Vibrator;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bean.AttandanceTransaction;
 import com.bean.User;
@@ -26,9 +28,14 @@ import com.listener.SaveAttandanceRequestListener;
 import com.services.SaveAttandanceRequest;
 import com.util.Util;
 
+import org.ndeftools.Message;
+import org.ndeftools.Record;
+import org.ndeftools.externaltype.AndroidApplicationRecord;
+
 import java.nio.charset.Charset;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import roboguice.inject.ContentView;
@@ -37,9 +44,10 @@ import roboguice.inject.InjectView;
 @ContentView(R.layout.activity_scan_card_batch)
 public class ScanCardBatchActivity extends BaseActivity {
 
+    private static final String TAG = ScanCardBatchActivity.class.getName();
 
     public static final String MIME_TEXT_PLAIN = "text/plain";
-    public static final String TAG = "NfcDemo";
+//    public static final String TAG = "NfcDemo";
 
     private NfcAdapter mNfcAdapter;
     private PendingIntent mPendingIntent;
@@ -57,11 +65,15 @@ public class ScanCardBatchActivity extends BaseActivity {
     @InjectView(R.id.inBUTTON)  Button inBUTTON;
     @InjectView(R.id.outBUTTON) Button outBUTTON;
 
+
+    protected NfcAdapter nfcAdapter;
+    protected PendingIntent nfcPendingIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_card_batch);
-        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+//        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Calibri.ttf");
         Typeface digital = Typeface.createFromAsset(getAssets(), "fonts/digital_7_mono.ttf");
 
@@ -81,15 +93,80 @@ public class ScanCardBatchActivity extends BaseActivity {
         hhET.setText(""+hour);
         mmET.setText(""+(min < 10 ? "0"+ min : min));
 
-        if (mNfcAdapter != null) {
-            Toast.makeText(this, "Read an NFC tag", Toast.LENGTH_SHORT).show();
+//        if (mNfcAdapter != null) {
+//            Toast.makeText(this, "Read an NFC tag", Toast.LENGTH_SHORT).show();
+//        } else {
+//            Toast.makeText(this, "This phone is not NFC enabled", Toast.LENGTH_SHORT).show();
+//        }
+//        mPendingIntent = PendingIntent.getActivity(this, 0,
+//                new Intent(this, ScanCardBatchActivity.class).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+//        mNdefPushMessage = new NdefMessage(new NdefRecord[] { newTextRecord(
+//                "Message from NFC Reader :-)", Locale.ENGLISH, true) });
+
+
+
+        // initialize NFC
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        nfcPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, this.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+    }
+
+    public void enableForegroundMode() {
+        Log.d(TAG, "enableForegroundMode");
+
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED); // filter for all
+        IntentFilter[] writeTagFilters = new IntentFilter[] {tagDetected};
+        nfcAdapter.enableForegroundDispatch(this, nfcPendingIntent, writeTagFilters, null);
+    }
+
+    public void disableForegroundMode() {
+        Log.d(TAG, "disableForegroundMode");
+
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        Log.d(TAG, "onNewIntent");
+
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
+            TextView textView = (TextView) findViewById(R.id.counterValTV);
+
+            textView.setText("Hello NFC!");
+
+            Parcelable[] messages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (messages != null) {
+
+                Log.d(TAG, "Found " + messages.length + " NDEF messages"); // is almost always just one
+
+                vibrate(); // signal found messages :-)
+
+                // parse to records
+                for (int i = 0; i < messages.length; i++) {
+                    try {
+                        List<Record> records = new Message((NdefMessage)messages[i]);
+
+                        Log.d(TAG, "Found " + records.size() + " records in message " + i);
+
+                        for(int k = 0; k < records.size(); k++) {
+                            Log.d(TAG, " Record #" + k + " is of class " + records.get(k).getClass().getSimpleName());
+
+                            Record record = records.get(k);
+
+                            if(record instanceof AndroidApplicationRecord) {
+                                AndroidApplicationRecord aar = (AndroidApplicationRecord)record;
+                                Log.d(TAG, "Package is " + aar.getDomain() + " " + aar.getType());
+                            }
+
+                        }
+                    } catch (Exception e) {
+                        Log.e(TAG, "Problem parsing message", e);
+                    }
+
+                }
+            }
         } else {
-            Toast.makeText(this, "This phone is not NFC enabled", Toast.LENGTH_SHORT).show();
+            // ignore
         }
-        mPendingIntent = PendingIntent.getActivity(this, 0,
-                new Intent(this, ScanCardBatchActivity.class).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-        mNdefPushMessage = new NdefMessage(new NdefRecord[] { newTextRecord(
-                "Message from NFC Reader :-)", Locale.ENGLISH, true) });
     }
 
     private NdefRecord newTextRecord(String text, Locale locale, boolean encodeInUtf8) {
@@ -112,19 +189,23 @@ public class ScanCardBatchActivity extends BaseActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        if (mNfcAdapter != null) {
-            mNfcAdapter.enableForegroundDispatch(this, mPendingIntent, null, null);
-            mNfcAdapter.setNdefPushMessage(mNdefPushMessage, this);
-        }
+//        if (mNfcAdapter != null) {
+//            mNfcAdapter.enableForegroundDispatch(this, mPendingIntent, null, null);
+//            mNfcAdapter.setNdefPushMessage(mNdefPushMessage, this);
+//        }
+
+        enableForegroundMode();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (mNfcAdapter != null) {
-            mNfcAdapter.disableForegroundDispatch(this);
-            mNfcAdapter.setNdefPushMessage(mNdefPushMessage, this);
-        }
+//        if (mNfcAdapter != null) {
+//            mNfcAdapter.disableForegroundDispatch(this);
+//           mNfcAdapter.setNdefPushMessage(mNdefPushMessage, this);
+//        }
+
+        disableForegroundMode();
     }
 
     private void showWirelessSettingsDialog() {
@@ -171,11 +252,11 @@ public class ScanCardBatchActivity extends BaseActivity {
         return result;
     }
 
-    @Override
-    public void onNewIntent(Intent intent) {
-        setIntent(intent);
-        resolveIntent(intent);
-    }
+//    @Override
+//    public void onNewIntent(Intent intent) {
+//        setIntent(intent);
+//        resolveIntent(intent);
+//    }
 
     public void save(View view) {
         String type = view.getId() == R.id.inBUTTON ? "IN" : "OUT";
@@ -214,6 +295,13 @@ public class ScanCardBatchActivity extends BaseActivity {
                 });
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
+    }
+
+    private void vibrate() {
+        Log.d(TAG, "vibrate");
+
+        Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE) ;
+        vibe.vibrate(500);
     }
 
 }
