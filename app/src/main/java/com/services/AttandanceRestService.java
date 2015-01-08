476aa61bbf5a63ad4e7d88b451d125e1785854ee
kanaiyathacker <kanaiyathacker@gmail.com>
2015-01-08 23:24:38 +0000

    New changelist

--- app/src/main/java/com/services/AttandanceRestService.java
+++ app/src/main/java/com/services/AttandanceRestService.java
@@ -6,8 +6,8 @@ import com.octo.android.robospice.retrofit.RetrofitGsonSpiceService;
  * Created by kanaiyalalt on 11/11/2014.
  */
 public class AttandanceRestService extends RetrofitGsonSpiceService {
-//    private final static String BASE_URL = "http://kanserviceslive.elasticbeanstalk.com";
-    private final static String BASE_URL = "http://192.168.0.3:8080/DeviceTokenApplicaion-0.0.1-SNAPSHOT";
+    private final static String BASE_URL = "http://kanserviceslive.elasticbeanstalk.com";
+//    private final static String BASE_URL = "http://192.168.0.3:8080/DeviceTokenApplicaion-0.0.1-SNAPSHOT";
 
     @Override
     public void onCreate() {
